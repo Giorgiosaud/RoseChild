41db2c883b7b567870a10bd8c8ffd7cc94dcb939
Jorge Saud <jorgelsaud@gmail.com>
2017-06-16 09:26:58 -0400

    fix logo loading

--- functions.php
+++ functions.php
@@ -1,4 +1,8 @@
 <?php
+define('CHILD_THEME_URI', get_stylesheet_directory_uri());
+define('CHILD_THEME_DIR', get_stylesheet_directory());
+define('CHILD_ADMIN_DIR', CHILD_THEME_DIR . '/admin');
+require_once CHILD_ADMIN_DIR.'/initialize.php';
 function my_theme_enqueue_styles() {
     $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' ,array('bootstrap','animate','simpletextrotator','rose-javascript','rose-main','rose-responsive'));
