8af4a138756c15971ff29c84d5a5b2e18b06954c
Jorge Saud <jorgelsaud@gmail.com>
2017-06-16 16:47:57 -0400

    fix logo loading

--- scss/style.scss.php
+++ scss/style.scss.php
@@ -1,6 +1,6 @@
 <?php $optionsChild=get_option('child_theme') ?>
 
-$imagen: <?php $imagen=wp_get_attachment_image($optionsChild['logo_cargador'],'full',false,array('class'=>'img-responsive logo-fixed on-pc'));echo $imagen; ?>;
+$imagen: <?php echo wp_get_attachment_image($optionsChild['logo_cargador'],'full',false,array('class'=>'img-responsive logo-fixed on-pc')); ?>;
 
 .theme{
 	background-image:url($imagen);
