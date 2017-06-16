258baeda041c2307c8250b04b029cf47c85b6b5f
Jorge Saud <jorgelsaud@gmail.com>
2017-06-16 10:16:18 -0400

    fix logo loading

--- admin/Page/child_options_settings.php
+++ admin/Page/child_options_settings.php
@@ -148,7 +148,7 @@ class MySettingsPage
     	if($imagen='')
     		echo '<img class="imagen_element imagen_upload" src="" />';
     	printf('<input class="logo_cargador_url" type="hidden" name="child_theme[logo_cargador]" value="%s">', $logoCargador);
-    	echo '<a href="#" class="imagen_upload" data-input-selector=".logo_cargador_url" data-image-selector=".imagen_upload">Upload</a>';
+    	echo '<a href="#" class="page-title-action aria-button-if-js imagen_upload" data-input-selector=".logo_cargador_url" data-image-selector=".imagen_upload">Upload</a>';
     }
 }
 
