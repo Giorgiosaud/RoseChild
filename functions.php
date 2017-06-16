123897a844851709e971a002a951a76b6862bb6c
Jorge Saud <jorgelsaud@gmail.com>
2017-06-16 09:14:34 -0400

    fix logo top

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
-define('CHILD_ADMIN_DIR', CHILD_THEME_DIR . '/admin');
-require_once CHILD_ADMIN_DIR.'/initialize.php';
+// define('CHILD_ADMIN_DIR', CHILD_THEME_DIR . '/admin');
+// require_once CHILD_ADMIN_DIR.'/initialize.php';
 function my_theme_enqueue_styles() {
     $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' ,array('bootstrap','animate','simpletextrotator','rose-javascript','rose-main','rose-responsive'));
