825394b8c14458bf59f2a48f2f51a9b0f1d9f186
Jorge Saud <jorgelsaud@gmail.com>
2017-06-16 09:22:13 -0400

    fix logo top

--- functions.php
+++ functions.php
@@ -1,10 +1,4 @@
 <?php
-// define('CHILD_THEME_DIR', get_stylesheet_directory());
-// echo CHILD_THEME_DIR;
-// define('CHILD_ADMIN_DIR', CHILD_THEME_DIR . '/admin');
-// define('CHILD_THEME_URI', get_stylesheet_directory_uri());
-// require_once CHILD_ADMIN_DIR.'/initialize.php';
-
 function my_theme_enqueue_styles() {
     $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' ,array('bootstrap','animate','simpletextrotator','rose-javascript','rose-main','rose-responsive'));
@@ -22,3 +16,9 @@ function my_theme_enqueue_styles() {
   ) ) ) );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+add_action( 'cmb2_init', 'digital_portfolio_metabox' );
+function digital_portfolio_metabox(){
+//	cmb2_update_option('8',rose_get_prefix('style'),'Only Content',false);
+//	$portfolioMB=cmb2_get_metabox('portfolio');
+//	$field=$portfolioMB->get_field( rose_get_prefix('style'));
+}
