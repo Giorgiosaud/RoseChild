b947033d5ca5abbee10047013f8a8b405e801755
Jorge Saud <jorgelsaud@gmail.com>
2017-06-16 17:02:05 -0400

    fix logo loading

--- scss/style.scss.php
+++ scss/style.scss.php
@@ -1,5 +1,5 @@
 
 .theme{
-	background-image:url('<?php echo get_option('child_theme','logo_cargador');
+	background-image:url('<?php echo get_option('child_theme')['logo_cargador'];
 	//wp_get_attachment_image_src(get_option('child_theme')['logo_cargador'],'full',false)['url']; ?>');
 }
