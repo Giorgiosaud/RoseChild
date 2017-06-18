1ad65c46028ac767bc7ba647081890c4627cdc9b
Jorge Saud <jorgelsaud@gmail.com>
2017-06-18 00:29:01 -0400

    Logo Top in mobile

--- scss/style.scss.php
+++ scss/style.scss.php
@@ -1,6 +1,12 @@
 $LogoLoader:"<?php echo wp_get_attachment_image_src(get_option('child_theme')['logo_cargador'],'full',false)[0]; ?>";
 
 
+
 div#status:before{
-	background-image:url($LogoLoader);
+	background-image:url( $LogoLoader );
+}
+@media only screen and (max-width: 1024px){
+	header.transparent .logo{
+		background-image: url( $LogoLoader ) !important;
+	}
 }
