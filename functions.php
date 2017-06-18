fa0209ffcd66cf9efc96f6ebdb8e07e49f2351a8
Jorge Saud <jorgelsaud@gmail.com>
2017-06-18 11:31:01 -0400

    Logo Top in mobile

--- functions.php
+++ functions.php
@@ -4,8 +4,8 @@ define('CHILD_THEME_DIR', get_stylesheet_directory());
 define('CHILD_ADMIN_DIR', CHILD_THEME_DIR . '/admin');
 require_once CHILD_ADMIN_DIR.'/initialize.php';
 require_once( 'wp-sass/wp-sass.php' );
-add_action( 'init', 'remove_action');
-function remove_action() {
+add_action( 'init', 'remove_parent_action');
+function remove_parent_action() {
   remove_action( 'wp_enqueue_scripts', 'rose_theme_scripts_styles' ,9);
 }
 function my_theme_enqueue_styles() {
