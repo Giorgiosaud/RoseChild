5f0acb393e0bf112e52f49aafdd9d165654197e0
Jorge Saud <jorgelsaud@gmail.com>
2017-06-19 10:14:03 -0400

    add flags icons

--- functions.php
+++ functions.php
@@ -32,6 +32,9 @@ function my_theme_enqueue_styles() {
         wp_get_theme()->get('Version')
         );
     wp_enqueue_style( 'scss-style', get_stylesheet_directory_uri() . '/scss/style.scss.php' );
+
+    wp_enqueue_style('flags-css','https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/2.8.0/css/flag-icon.css',array('scss-style'));
+
     wp_enqueue_script( 'jquery.flexslider', get_template_directory_uri() .'/assets/js/jquery.flexslider.js', array('jquery'), false, true );
     wp_enqueue_script( 'parallax', get_template_directory_uri() .'/assets/js/jquery.parallax-1.1.3.js', array('jquery'), '1.1.3', true );
     wp_enqueue_script( 'scrollto', get_template_directory_uri() .'/assets/js/jquery.scrollto-1.4.3.1.js', array('jquery'), '1.4.3.1', true );
