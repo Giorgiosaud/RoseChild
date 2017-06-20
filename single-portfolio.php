bbbdc0fcb3d0b4c2896df29fd4185bd6f15d323a
Jorge Saud <jorgelsaud@gmail.com>
2017-06-20 08:45:31 -0400

    fix nav

--- single-portfolio.php
+++
@@ -1,8 +0,0 @@
-<?php get_header();
-
-while(have_posts()) : the_post();
-    get_template_part( 'partials/portfolio/single/style', apply_filters('portfolio-style', 1) );
-endwhile;wp_reset_postdata();
-
-get_footer(); 
-?>
