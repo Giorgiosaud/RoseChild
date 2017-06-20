3f93653c165f33603f53a9db69c4cdf46056339c
Jorge Saud <jorgelsaud@gmail.com>
2017-06-20 17:14:13 -0400

    fix portfolio

--- single-portfolio.php
+++ single-portfolio.php
@@ -3,7 +3,7 @@
 while(have_posts()) : the_post();
 
 get_template_part( 'partials/portfolio/single/style', apply_filters('portfolio-style', 1) );
-
+comments_template();
 endwhile;wp_reset_postdata();
 
 get_footer(); ?>
