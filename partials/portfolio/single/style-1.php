fc6205467721ced5acd4737f93ee695c62a420f2
Jorge Saud <jorgelsaud@gmail.com>
2017-06-21 09:43:13 -0400

    fix portfolio

--- partials/portfolio/single/style-1.php
+++ partials/portfolio/single/style-1.php
@@ -19,7 +19,7 @@
             }
             elseif(in_array($ext,$extvideo)){
                 ?>
-                <video class="scale video-js" data-setup='{"controls": false, "autoplay": true, "preload": "auto","loop":true}'>
+                <video class="scale video-js" data-setup='{"controls": true, "autoplay": false, "preload": "auto","loop":true}'>
                     <source src ="<?php echo esc_url( $image );?>" alt="<?php esc_html_e('Project Media','rose');?>" />
                     </video>
                     <?php
