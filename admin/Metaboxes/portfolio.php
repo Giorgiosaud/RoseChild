7a6733f8150cf326316a0f88b903720ab94c9058
Jorge Saud <jorgelsaud@gmail.com>
2017-06-21 13:56:46 -0400

    metabox

--- admin/Metaboxes/portfolio.php
+++ admin/Metaboxes/portfolio.php
@@ -1,23 +1,28 @@
 <?php 
-add_action( 'cmb2_init', 'rose_child_portfolio_metabox' );
+add_action( 'cmb2_admin_init', 'rose_child_portfolio_metabox' );
 /**
  * Hook in and add a demo metabox. Can only happen on the 'cmb2_init' hook.
  */
 function rose_child_portfolio_metabox(){
-
+// Start with an underscore to hide fields from custom fields list
+	$prefix = '_zonapro_';
 	/**
 	 * Sample metabox to demonstrate each field type included
 	 */
 	$cmb = new_cmb2_box( array(
-		'id'            => rose_get_prefix('links-metabox'),
-		'title'         => esc_html__( 'External Embedable Links', 'rose' ),
-		'object_types'  => array( 'page', 'portfolio' ), // Post type
-		// 'show_on_cb' => 'rose_show_if_front_page', // function should return a bool value
-		) );
+		'id'            => 'links_metabox',
+		'title'         => __( 'External Embendable Links', 'zonapro' ),
+		'object_types'  => array( 'portfolio', ), // Post type
+		'context'       => 'normal',
+		'priority'      => 'high',
+		'show_names'    => true, // Show field names on the left
+		// 'cmb_styles' => false, // false to disable the CMB stylesheet
+		// 'closed'     => true, // Keep the metabox closed by default
+	) );
 	$group_field_id = $cmb->add_field( array(
-		'id'          => 'wiki_test_repeat_group',
+		'id'          => $prefix.'embeds_group',
 		'type'        => 'group',
-		'description' => __( 'Generates reusable form entries', 'cmb2' ),
+		'description' => __( 'Generate a new oembed', 'cmb2' ),
 	// 'repeatable'  => false, // use false if you want non-repeatable group
 		'options'     => array(
 		'group_title'   => __( 'Entry {#}', 'cmb2' ), // since version 1.1.4, {#} gets replaced by row number
@@ -29,7 +34,7 @@ function rose_child_portfolio_metabox(){
 		) );
 	$cmb->add_group_field( $group_field_id, array(
 		'name' => 'Link oEmbed',
-		'id'   => 'oembed',
+		'id'   => $prefix.'oembed',
 		'desc' => 'Enter a youtube, twitter, or instagram URL. Supports services listed at <a href="http://codex.wordpress.org/Embeds">http://codex.wordpress.org/Embeds</a>.',
 		'type' => 'oembed',
 	// 'repeatable' => true, // Repeatable fields are supported w/in repeatable groups (for most types)
