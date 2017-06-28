2a722d4ceecd500e8189887b318edc79b59c5b79
Jorge Saud <jorgelsaud@gmail.com>
2017-06-28 14:28:50 -0400

    fix metaboxes

--- partials/portfolio/single/style-1.php
+++ partials/portfolio/single/style-1.php
@@ -33,8 +33,9 @@
 
                 <?php $i++; endforeach;
                 $embeds=rose_get_meta( get_the_ID(), 'embeds_group');
-                var_dump($embeds);
+
                 foreach ( (array) $embeds as $key => $embed ) {
+                var_dump($embed);
                     ?>
                     <div class="spacer"></div>
                     <?php
